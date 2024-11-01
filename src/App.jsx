1c84cc031201de8edd5416cd4128b00f1bb96830
Janriz Libres <libres.janriz@gmail.com>
2024-11-01 09:57:19 +0800

    add sidebar, home screen, and add project screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
+import NoProjectScreen from "./components/NoProjectScreen";
 
 function App() {
+  const [addProject, setAddProject] = useState(false);
+
+  function handleAddProject() {
+    setAddProject(true);
+  }
+
   return (
-    <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-    </main>
+    <>
+      <main className="h-screen my-8 flex gap-8">
+        <Sidebar />
+        {addProject ? <NewProject /> : <NoProjectScreen />}
+      </main>
+    </>
   );
 }
 
