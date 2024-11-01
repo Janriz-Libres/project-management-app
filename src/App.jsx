819c7643a9e14d2f41238cd7ffa85ebf64a526fa
Janriz Libres <libres.janriz@gmail.com>
2024-11-01 10:13:35 +0800

    conditional rendering of home and add project screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
 import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
-import NoProjectScreen from "./components/NoProjectScreen";
+import HomeScreen from "./components/HomeScreen";
 
 function App() {
-  const [addProject, setAddProject] = useState(false);
+  const [projects, setProjects] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
 
   function handleAddProject() {
-    setAddProject(true);
+    setProjects((prevState) => ({
+      selectedProject: null,
+      projects: [...prevState.projects],
+    }));
+  }
+
+  function handleCancel() {
+    setProjects((prevState) => ({
+      selectedProject: undefined,
+      projects: [...prevState.projects],
+    }));
   }
 
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar />
-        {addProject ? <NewProject /> : <NoProjectScreen />}
+        <Sidebar onAddProject={handleAddProject} />
+        {projects.selectedProject === undefined && (
+          <HomeScreen onAddProject={handleAddProject} />
+        )}
+        {projects.selectedProject === null && (
+          <NewProject onCancel={handleCancel} />
+        )}
       </main>
     </>
   );
